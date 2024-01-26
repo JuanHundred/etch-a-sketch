035a21646018586c70b375454a796a116f3bfffb
JuanHundred <juanquezada336@gmail.com>
2024-01-26 18:17:48 -0500

    Adds functionality to change grid size and colors when clicking buttons

--- index.js
+++ index.js
@@ -4,13 +4,32 @@ const grid = document.querySelector("#grid");
 // create div that will become the cell for the grid
 const cell = document.createElement("div");
 
+// buttons below
+const smallButton = document.querySelector(".small");
+const mediumButton = document.querySelector(".medium");
+const largeButton = document.querySelector(".large");
+
+const regularButton = document.querySelector(".regular-color");
+const multiColorButton = document.querySelector(".multi-color");
+
+const erase = document.querySelector(".erase");
+
+const gridButtons = {
+    types: ["small", "regular"]
+}
+
+const gridSize = {
+    "small": [16, 0],
+    "medium": [32, 1],
+    "large": [64, 2] 
+}
+
 // create cell depending on the size of the grid the user wants
 function createCell(dimension) {
     const dimensions = (704 / dimension);
     cell.classList.add("cell");
     cell.style.width = `${dimensions}px`;
     cell.style.height = `${dimensions}px`;
-    
 }
 
 // generate random color
@@ -18,17 +37,24 @@ function generateRandomColor() {
     return '#' + Math.floor(Math.random()*16777215).toString(16);
 }
 
-
 // add event listeners to every clone cell created, as cloning nodes doesn't copy event listeners 
 function addListeners(parentNode){
     if (parentNode.hasChildNodes()){
         let children = parentNode.childNodes;
         for(const node of children){
             node.addEventListener("mouseenter", () => {
-                node.style.backgroundColor = "#808080";
+                if (gridButtons.types[1] === "regular"){
+                    node.style.backgroundColor = "#808080";
+                } else {
+                    node.style.backgroundColor = `${generateRandomColor()}`;
+                }
             });
             node.addEventListener("mouseout", () => {
-                node.style.backgroundColor = "#808080";
+                if (gridButtons.types[1] === "regular"){
+                    node.style.backgroundColor = "#808080";
+                } else {
+                    node.style.backgroundColor = `${generateRandomColor()}`;
+                }
             });
         }
     }
@@ -48,18 +74,25 @@ function createRow(cellMultiplier, node) {
     }
 }
 
+function removeAllChildNodes(parentNode) {
+    while (parentNode.firstChild) {
+        parentNode.removeChild(parentNode.firstChild);
+    }
+}
+
 // generates all the cells that will be in the grid
-function createGrid(dimension){
+function createGrid(dimension, gridSize, color){
+    removeAllChildNodes(row);
+    removeAllChildNodes(grid);
     // create the cell
     createCell(dimension);
     // add the class name to make the div flex
     row.classList.add("row");
 
-    createRow(0, row);
-    
+    createRow(gridSize, row);
     for(i = 0; i < dimension; i++){
         const rowClone = row.cloneNode(true);
-        addListeners(rowClone);
+        addListeners(rowClone, color);
         grid.appendChild(rowClone);
     }
     
@@ -76,5 +109,33 @@ function createGrid(dimension){
 //         });
 //     });
 // }
-createGrid(16);
-addListeners();
+createGrid(16, 0);
+smallButton.addEventListener("click", () => {
+    gridButtons.types[0] = "small";
+    createGrid(gridSize[gridButtons['types'][0]][0], gridSize[gridButtons['types'][0]][1]);
+});
+
+mediumButton.addEventListener("click", () => {
+    gridButtons.types[0] = "medium";
+    createGrid(gridSize[gridButtons['types'][0]][0], gridSize[gridButtons['types'][0]][1]);
+});
+
+largeButton.addEventListener("click", () => {
+    gridButtons.types[0] = "large";
+    createGrid(gridSize[gridButtons['types'][0]][0], gridSize[gridButtons['types'][0]][1]);
+});
+
+regularButton.addEventListener('click', () => {
+    gridButtons.types[1] = "regular";
+    createGrid(gridSize[gridButtons['types'][0]][0], gridSize[gridButtons['types'][0]][1]);
+});
+
+multiColorButton.addEventListener('click', () => {
+    gridButtons.types[1] = "colors";
+    createGrid(gridSize[gridButtons['types'][0]][0], gridSize[gridButtons['types'][0]][1]);
+});
+
+
+erase.addEventListener("click", () => {
+    
+});
